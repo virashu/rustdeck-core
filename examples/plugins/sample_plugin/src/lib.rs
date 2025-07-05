236fb7043a3fbe10f8728f70c348b39ab6084001
virashu <virashu@disroot.org>
2025-07-05 15:43:18 +0300

    feat: add builder example to sample plugin

--- examples/plugins/sample_plugin/src/lib.rs
+++ examples/plugins/sample_plugin/src/lib.rs
@@ -1,10 +1,7 @@
 #![allow(clippy::unnecessary_wraps)]
 #![allow(unsafe_op_in_unsafe_fn)]
 
-use rustdeck_common::{
-    Args, actions, args, decl_action, decl_arg, decl_plugin, decl_variable, export_plugin,
-    variables,
-};
+use rustdeck_common::{Args, export_plugin};
 
 struct PluginState {
     counter: i32,
@@ -46,47 +43,87 @@ fn run_action(
     Ok(())
 }
 
-export_plugin! {
-    decl_plugin! {
-        id: "plugin_test",
-        name: "Sample Plugin",
-        desc: "A sample plugin",
-        variables: variables!(
-            decl_variable! {
-                id: "counter",
-                desc: "Counter",
-                vtype: "string",
-            },
-        ),
-        actions: actions!(
-            decl_action! {
-                id: "increment",
-                name: "Increment",
-                desc: "Increment counter",
-            },
-            decl_action! {
-                id: "add",
-                name: "Add",
-                desc: "Add value to counter",
-                args: args!(
-                    decl_arg! {
-                        id: "amount",
-                        name: "Amount",
-                        desc: "Amount",
-                        vtype: "int",
-                    },
-                ),
-            },
-            decl_action! {
-                id: "clear",
-                name: "Clear",
-                desc: "Set counter value to 0",
-            },
-        ),
+/*
+ * Definition using macros
+ */
 
-        fn_init: init,
-        fn_update: update,
-        fn_get_variable: get_variable,
-        fn_run_action: run_action,
-    }
+// use rustdeck_common::{
+//     actions, args, decl_action, decl_arg, decl_plugin, decl_variable, variables,
+// };
+//
+// export_plugin! {
+//     decl_plugin! {
+//         id: "plugin_test",
+//         name: "Sample Plugin",
+//         desc: "A sample plugin",
+//         variables: variables!(
+//             decl_variable! {
+//                 id: "counter",
+//                 desc: "Counter",
+//                 vtype: "string",
+//             },
+//         ),
+//         actions: actions!(
+//             decl_action! {
+//                 id: "increment",
+//                 name: "Increment",
+//                 desc: "Increment counter",
+//             },
+//             decl_action! {
+//                 id: "add",
+//                 name: "Add",
+//                 desc: "Add value to counter",
+//                 args: args!(
+//                     decl_arg! {
+//                         id: "amount",
+//                         name: "Amount",
+//                         desc: "Amount",
+//                         vtype: "int",
+//                     },
+//                 ),
+//             },
+//             decl_action! {
+//                 id: "clear",
+//                 name: "Clear",
+//                 desc: "Set counter value to 0",
+//             },
+//         ),
+
+//         fn_init: init,
+//         fn_update: update,
+//         fn_get_variable: get_variable,
+//         fn_run_action: run_action,
+//     }
+// }
+
+/*
+ * Definition using builder pattern
+ */
+
+use rustdeck_common::{
+    Type,
+    builder::{Action, PluginBuilder, Variable},
+    decorate_fn_get_variable, decorate_fn_init, decorate_fn_run_action, decorate_fn_update,
+};
+
+export_plugin! {
+    PluginBuilder::new("plugin_test", "Sample Plugin", "A sample plugin")
+        .init(decorate_fn_init!(init))
+        .update(decorate_fn_update!(update))
+        .get_variable(decorate_fn_get_variable!(get_variable))
+        .run_action(decorate_fn_run_action!(run_action))
+
+        /* Variables */
+        .variable(Variable::new("counter", "Counter", Type::String))
+
+        /* Actions */
+        .action(Action::new("increment", "Increment", "Increment counter"))
+        .action(
+            Action::new("add", "Add", "Add value to counter")
+                .arg("amount", "Amount", "Amount", Type::Int)
+        )
+        .action(Action::new("clear", "Clear", "Set counter value to 0"))
+
+        .build()
+        .unwrap()
 }
